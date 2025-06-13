db01812800d655af7160bd7a879662922918abb3
Himanshu Tiwari <himanshut1806@gmail.com>
2025-06-13 12:55:48 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ def projects(request):
 
         {
             'title':'Ecommerce',
-            'path':'images/ecommerce.PNG',
+            'path':'images/ecommerce.png',
         },
 
         {
